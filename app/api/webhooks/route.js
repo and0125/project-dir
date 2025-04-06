3a1313afe7569e038a1e307629811051b8ffc910
Aaron Dantley <aarondantle@gmail.com>
2025-04-05 21:34:07 -0400

    feat: updating webhooks api route name to make sure it syncs with clerk

--- app/api/webhooks/route.js
+++ app/api/webhooks/route.js
@@ -3,6 +3,7 @@ import { headers } from "next/headers";
 import { createOrUpdateUser, deleteUser } from "@/lib/actions/user";
 
 export async function POST(req) {
+  console.log("Mongodb route being utilized...");
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
